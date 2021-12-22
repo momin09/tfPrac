4350be0a15bbc7b50afd2365133245782c40cbcd
momin09 <khm0909z@gmail.com>
2021-12-23 02:24:29 +0900

    Add: variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
-variables "aws_region" {
+variable "aws_access_key" {
+    description = "USER's access KEY in csv"
+}
+
+variable "aws_secret_key" {
+    description = "USER's secret KEY in csv"
+}
+
+variable "aws_region" {
     description = "region for aws"
 }
