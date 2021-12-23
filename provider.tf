d627a9217cbec8bb109a071e5d72005eabec4725
momin09 <khm0909z@gmail.com>
2021-12-24 01:22:44 +0900

    Update: VPC line

--- provider.tf
+++ provider.tf
@@ -4,7 +4,58 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "testingEC2" {
-  ami = "ami-0e17ad9abf7e5c818"
-  instance_type = "t2.micro"
+resource "aws_vpc" "prac_VPC" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tfPrac-VPC"
+  }
+}
+
+resource "aws_subnet" "prac_public_subnet" {
+  vpc_id = aws_vpc.prac_VPC.id
+  cidr_block = "10.0.1.0/24"
+
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "tfPrac-subnet1"
+  }
+}
+
+resource "aws_subnet" "prac_private_subnet" {
+  vpc_id = aws_vpc.prac_VPC.id
+  cidr_block = "10.0.2.0/24"
+
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "tfPrac-subnet2"
+  }
+}
+
+resource "aws_internet_gateway" "prac_igw" {
+  vpc_id = aws_vpc.prac_VPC.id
+
+  tags = {
+    Name = "tfPrac-igw"
+  }
+}
+
+resource "aws_eip" "prac_elasticIP" {
+  vpc = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_nat_gateway" "prac_natG" {
+  allocation_id = aws_eip.prac_elasticIP.id
+
+  subnet_id = aws_subnet.prac_public_subnet.id
+
+  tags = {
+    Name = "tfPrac-natGateway"
+  }
 }
